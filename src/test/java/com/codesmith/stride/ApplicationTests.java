2d92238eea238048aab2ecf10de15a2cea26b950
nathsagar96 <nathsagar96@gmail.com>
2024-12-11 22:30:53 +0530

    first commit

---
+++ src/test/java/com/codesmith/stride/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesmith.stride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestContainersConfiguration.class)
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ src/test/java/com/codesmith/stride/TestContainersConfiguration.java
@@ -0,0 +1,17 @@
+package com.codesmith.stride;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestContainersConfiguration {
+
+  @Bean
+  @ServiceConnection
+  PostgreSQLContainer<?> postgresContainer() {
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
+  }
+}
